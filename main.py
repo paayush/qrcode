d9d099752ffeecf8e80628ed9bcb00c049a08c52
Aayush pokharel <pokharelaayush199@gmail.com>
2022-08-18 20:20:38 +0545

    co degenerate

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-'''import qrcode
-fb_id = "https://www.facebook.com/photo/?fbid=344086437918224&set=a.103390275321176"
-
-#import image
-qrcode.make(fb_id).save(ram.png)'''
-
-'''import qrcode
-
-id = "100069506107789"
-fb_url = f"https://www.facebook.com/people/Ram.Pariyar/{id}"
-qrcode.make(fb_url).save(ram.png)'''
-
-'''import random
-
-for i in range(1,9+1):
-    num = random.randint(1,9)
-    last_num = id[14]
-    last_num = f"{id[0:14]+str[num]}"'''
-#qrcode.make(last_num).save(f"{i}.png")
 
 # Import QRCode from pyqrcode
 import pyqrcode
